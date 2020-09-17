29779ddc1067f763c64a7e3cd426b4ed3125e76a
Nayeem Jahan Rafi <nayeem.jr@samsung.com>
2020-09-17 16:57:10 +0600

    New Fav button logic Updated

--- app/src/main/java/com/example/gameapp/ui/gameDetails/GameDetailsFragment.kt
+++ app/src/main/java/com/example/gameapp/ui/gameDetails/GameDetailsFragment.kt
@@ -1,7 +1,6 @@
 package com.example.gameapp.ui.gameDetails
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,33 +34,28 @@ class GameDetailsFragment : Fragment() {
         }
         binding.viewModel = gameDetailsViewModel
         binding.lifecycleOwner = this
-        //binding.favoriteButton.visibility = View.GONE
+        binding.favoriteButton.visibility = View.GONE
 
         setupFavoriteButton(binding)
         return binding.root
     }
 
     private fun setupFavoriteButton(binding: FragmentGameDetailsBinding) {
-//        binding.favoriteButton.setOnLikeListener(object : OnLikeListener {
-//            override fun liked(likeButton: LikeButton?) {
-//                gameDetailsViewModel.addToFavorite()
-//            }
-//
-//            override fun unLiked(likeButton: LikeButton?) {
-//                gameDetailsViewModel.removeFromFavourite()
-//            }
-//        })
-
-//        gameDetailsViewModel.favoriteGames.observe(viewLifecycleOwner, {
-//            // TODO: Update to individual function
-//            if (binding.favoriteButton.visibility == View.GONE) {
-//                binding.favoriteButton.visibility = View.VISIBLE
-//                binding.favoriteButton.isLiked = false
-//                for (game in it) {
-//                    if (game.id == gameId) binding.favoriteButton.isLiked = true
-//                }
-//            }
-//        })
+        binding.favoriteButton.setOnCheckStateChangeListener { _, checked ->
+            if(checked) gameDetailsViewModel.addToFavorite()
+            else gameDetailsViewModel.removeFromFavourite()
+        }
+        gameDetailsViewModel.favoriteGames.observe(viewLifecycleOwner, {
+            // TODO: Update to individual function
+            if (binding.favoriteButton.visibility == View.GONE) {
+                binding.favoriteButton.visibility = View.VISIBLE
+                var buttonNewStatus = false
+                for (game in it) {
+                    if (game.id == gameId) buttonNewStatus = true
+                }
+                binding.favoriteButton.isChecked = buttonNewStatus
+            }
+        })
 
     }
 }
